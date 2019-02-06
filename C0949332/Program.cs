ec1802bf84ea797eb55c46621575101db49965fa
peter sigurdson <peter@petersigurdson.net>
2019-02-06 10:44:24 -0500

    Add project files.

---
+++ C0949332/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace C0949332
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
